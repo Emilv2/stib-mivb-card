7298d9b9cb7152845de826c304c907944acb3ec4
Emil Vanherp <emil@vanherp.me>
2020-03-13 01:09:30 +0100

    show two arrows when bus/tram/metro is arriving
    like the official app and boards

--- stib-mivb-card.js
+++ stib-mivb-card.js
@@ -186,8 +186,11 @@ class STIBMIVBCard extends HTMLElement {
       cell = `<td class="${field}"><ha-icon icon="mdi:${passage[field]}"></ha-icon></td>`;
     }
     else if (field == 'waiting_time') {
-      if (passage[field] != null) {
-        cell = `<td class="${field}">${passage[field]}${STIBMIVBCard.SP}${STIBMIVBCard.MIN}</td>`;
+      if (passage[field] <= 0) {
+        cell = `<td class="${field}">⇊</td>`;
+      }
+      else if (passage[field] != null) {
+        cell = `<td class="${field}">${String(passage[field])}${STIBMIVBCard.SP}${STIBMIVBCard.MIN}</td>`;
       }
       else if (passage['next_passing_message']) {
         cell = `<td class="${field}">${passage['next_passing_message']}</td>`;
